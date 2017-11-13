b284b7e08f16001965db50a3600a2ad8071b691a
Ludwig Fuhr <ludwig.fuhr@yahoo.de>
2017-11-13 20:01:25 +0100

    Fix MikroVerkehr

--- MikroVerkehr.m
+++ MikroVerkehr.m
@@ -1,21 +1,23 @@
+clear;
+global LEER vMax zellen;
 zellen = 300;
 vMax = 5;
-dichte = .16;
+dichte = .2;
 LEER = -1;
 iterationen = 100;
-troedelwsnlkt =  0.2;
+troedelwsnlkt =  0;
 
 idxmod = @(x, indexRange) mod(x - 1, indexRange) + 1;
 
-
 strasse = zeros(1,zellen) - 1;
-while (sum(strasse) < dichte * zellen)
+while (sum(strasse ~= -1) < dichte * zellen)
     strasse(randi(zellen)) = randi(vMax + 1) - 1;
 end
 
-dsp(strasse);
+sss = strasse;
 
 for i = 1:iterationen
+    size(find(strasse+1))
     neueStrasse = zeros(1,zellen) - 1;
     for zelle = 1:zellen
         v = strasse(zelle);
@@ -23,36 +25,29 @@ for i = 1:iterationen
             % Beschleunigen
             v = min(v+1, vMax);
             % Bremsen
-            davor = find(strasse(idxmod(zelle+1:zelle+vMax, vMax))+1);
+            davor = find(strasse(idxmod(zelle+1:zelle+vMax, zellen))+1);
             if ~ isempty(davor)
-                v = min(v, davor(1));
+                v = min(v, davor(1) - 1);
             end
             % Trödeln
-            v = v - (rand()+troedelwsnlkt > 1);
+            v = v - (v ~= 0 && rand()+troedelwsnlkt > 1);
             % Bewegen
-            neueStrasse(idxmod(zelle+v,vMax)) = v;
+            neueStrasse(idxmod(zelle+v,zellen)) = v;
         end
     end
     strasse = neueStrasse;
-    
-    %pause(.2)
+    dsp(strasse)
+    pause(.2)
 end
 
 function result = farbe(wert)
-    disp(wert);
-    global LEER; global vMax;
-    
-    if wert == LEER
-        result = .9;
-    else
-        result = (wert * 0.6 / vMax);
-    end
+    global LEER vMax;  
+    result = (wert == LEER) .* 1 + (wert ~= LEER) .* (wert * 0.6 / vMax);
+    result = [result; result; result];
 end
 
 function dsp(strasse)
     global zellen;
-    triple = @(x) [x x x];
-    size(triple(arrayfun(farbe,strasse)'))
     scatter(cos((1:zellen)*2*pi/zellen), sin((1:zellen)*2*pi/zellen),[] ...
-        ,triple(arrayfun(farbe(strasse)))');
+        , farbe(strasse)' );
 end
